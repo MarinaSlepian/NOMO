d68ea1ab356f3116e5695d37534f1abb66902b30
Marina <slepianmarina@gmail.com>
2025-04-19 21:25:40 +0300

    Multi-Lang pref for AppChooser buttons and Slashes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { AppChooserComponent } from "../buttons/app-chooser/app-chooser.component";
-
+import { TranslateService,TranslateModule } from '@ngx-translate/core';
+import { SettingsDialogComponent } from '../settings-dialog/settings-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
-    imports: [AppChooserComponent]
+    imports: [AppChooserComponent, TranslateModule, MatDialogModule ]
   })
 
 
@@ -17,16 +21,45 @@ export class HeaderComponent
   @Output() selectNewApp = new EventEmitter<string>();
 
   selectedApp = "1";
+  selectedSplash = "1ru";
+  selectedLang = 'ru';
+
+  constructor(private translate: TranslateService, private dialog: MatDialog) 
+  {
+    this.translate.setDefaultLang('ru'); // sets fallback if no translation is found
+    this.translate.use('ru'); // sets the active language
+    this.selectedSplash = this.selectedApp+'ru';  
+    this.selectedLang = 'ru';   
+  }
+  
 
   selectApp(id: string)
   {
-    console.log('HeaderComponent - selectApp - id '+ id);
     this.selectedApp = id;
+    
+    this.updateSplashForLanguage();
     this.selectNewApp.emit(id); 
   }
-  onSettingsButton()
+
+  onSettingsButton(): void 
   {
-    
+    const dialogRef = this.dialog.open(SettingsDialogComponent, {
+      width: '300px'});
+  
+    dialogRef.afterClosed().subscribe((selectedLang: string | undefined) => {
+      if (selectedLang) {
+        console.log('Language changed to:', selectedLang);
+        // ✅ Add logic to update image or do anything else here
+        this.selectedLang = selectedLang;
+        this.updateSplashForLanguage();
+      }
+    });
   }
+
+  updateSplashForLanguage()
+  {
+    this.selectedSplash = this.selectedApp + this.selectedLang;  
+  }
+  
 
 } 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header/header.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideTranslate } from './app/translate.provider';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // ✅ Import this
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideTranslate(), provideAnimationsAsync() // ✅ Add this line!
+  ]
+});
