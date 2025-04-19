281c3bb5765b7bcb359d0e915e544785cacf61a0
Marina <slepianmarina@gmail.com>
2025-04-19 10:03:57 +0300

    ACTIONS ADDED

---
+++ src/app/buttons/buttons-actions-icons.ts
@@ -0,0 +1,255 @@
+export const BUTTONS_ACTIONS_ICONS = [ 
+    //MAX 20 VIDEOS PER BUTTON
+    {
+      id: '1',//LAUGH
+      name: '0',
+      avatar: 'user-1.png',
+      numVideos: '5',
+    },
+    {
+      id: '2', //CRY
+      name: '0',
+      avatar: 'user-2.png',
+      numVideos: '3',
+    },
+    {
+      id: '3',//DRINK
+      name: '0',
+      avatar: 'user-3.png',
+      numVideos: '7',
+    },
+    {
+      id: '4',//RUN
+      name: '0',
+      avatar: 'user-4.png',
+      numVideos: '7',
+    },
+    {
+      id: '5',//JUMP
+      name: '0',
+      avatar: 'user-5.png',
+      numVideos: '6',
+    },
+    {
+      id: '6',//KISS
+      name: '0',
+      avatar: 'user-6.png',
+      numVideos: '3',
+    },
+    {
+      id: '7',//YAWN
+      name: '0',
+      avatar: 'user-7.png',
+      numVideos: '3',
+    },
+    {
+      id: '8',//SING
+      name: '0',
+      avatar: 'user-8.png',
+      numVideos: '4',
+    },
+    {
+      id: '9',//SLEEP
+      name: '0',
+      avatar: 'user-9.png',
+      numVideos: '5',
+    },
+    {
+      id: '10',//EAT
+      name: '0',
+      avatar: 'user-10.png',
+      numVideos: '16',
+    },
+    {
+      id: '11',//SWIM
+      name: '0',
+      avatar: 'user-11.png',
+      numVideos: '5',
+    },
+    {
+      id: '12',//SMELL
+      name: '0',
+      avatar: 'user-12.png',
+      numVideos: '7',
+    },
+    {
+      id: '13',//SPIN 
+      name: '0',
+      avatar: 'user-13.png',
+      numVideos: '4',
+    },
+    {
+      id: '14',//SNEEZE
+      name: '0',
+      avatar: 'user-14.png',
+      numVideos: '4',
+    },
+    {
+      id: '15',//TURN OFF
+      name: '0',
+      avatar: 'user-15.png',
+      numVideos: '3',
+    },
+    {
+      id: '16',//SWING
+      name: '0',
+      avatar: 'user-16.png',
+      numVideos: '5',
+    },
+    {
+      id: '17',//SEARCH
+      name: '0',
+      avatar: 'user-17.png',
+      numVideos: '4',
+    },
+    {
+      id: '18',//PAINT
+      name: '0',
+      avatar: 'user-18.png',
+      numVideos: '5',
+    },
+    {
+      id: '19',//POUR
+      name: '0',
+      avatar: 'user-19.png',
+      numVideos: '5',
+    },
+    {
+      id: '20',//TURN ON
+      name: '0',
+      avatar: 'user-20.png',
+      numVideos: '5',
+    },
+    {
+      id: '21',//READ
+      name: '0',
+      avatar: 'user-21.png',
+      numVideos: '5',
+    },
+    {
+      id: '22',//OPEN
+      name: '0',
+      avatar: 'user-22.png',
+      numVideos: '10',
+    },
+    {
+      id: '23',//PULL
+      name: '0',
+      avatar: 'user-23.png',
+      numVideos: '4',
+    },
+    {
+      id: '24',//THROW
+      name: '0',
+      avatar: 'user-24.png',
+      numVideos: '6',
+    },
+    {
+      id: '25',//CRAWL
+      name: '0',
+      avatar: 'user-25.png',
+      numVideos: '4',
+    },
+    {
+      id: '26',//BLOW
+      name: '0',
+      avatar: 'user-26.png',
+      numVideos: '8',
+    },
+    {
+      id: '27',//PUSH
+      name: '0',
+      avatar: 'user-27.png',
+      numVideos: '2',
+    },
+    {
+      id: '28',//PET
+      name: '0',
+      avatar: 'user-28.png',
+      numVideos: '2',
+    },
+    {
+      id: '29',//FLY
+      name: '0',
+      avatar: 'user-29.png',
+      numVideos: '4',
+    },
+    {
+      id: '30',//BRUSH TEETH
+      name: '0',
+      avatar: 'user-30.png',
+      numVideos: '6',
+    },
+    {
+      id: '31',//WATER
+      name: '0',
+      avatar: 'user-31.png',
+      numVideos: '4',
+    },
+    {
+      id: '32',//DANCE 
+      name: '0',
+      avatar: 'user-32.png',
+      numVideos: '4',
+    },
+    {
+      id: '33',//GO DOWN
+      name: '0',
+      avatar: 'user-33.png',
+      numVideos: '4',
+    },
+    {
+      id: '34',//HUG
+      name: '0',
+      avatar: 'user-34.png',
+      numVideos: '3',
+    },
+    {
+      id: '35',//LIGHT UP
+      name: '0',
+      avatar: 'user-35.png',
+      numVideos: '3', 
+    },
+    {
+      id: '36',//LICK
+      name: '0',
+      avatar: 'user-36.png',
+      numVideos: '3',
+    },
+    {
+      id: '37',//EXTINGUISH
+      name: '0',
+      avatar: 'user-37.png',
+      numVideos: '4',
+    },
+    {
+      id: '38',//GO UP
+      name: '0',
+      avatar: 'user-38.png',
+      numVideos: '5',
+    },
+    {
+      id: '39',//CUT
+      name: '0',
+      avatar: 'user-39.png',
+      numVideos: '5',
+    },
+    {
+      id: '40',//BRUSH HAIR
+      name: '0',
+      avatar: 'user-40.png',
+      numVideos: '5',
+    },
+    {
+      id: '41',//CATCH
+      name: '0',
+      avatar: 'user-41.png',
+      numVideos: '3',
+    },
+    {
+      id: '42',//CLOSE
+      name: '0',
+      avatar: 'user-42.png',
+      numVideos: '7',
+    },
+  ];
