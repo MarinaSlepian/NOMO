d9c545079b8dfdb4177abb9a6608f3f696ae7a38
Marina <slepianmarina@gmail.com>
2025-04-24 15:36:43 +0300

    PWA behavior

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideTranslate } from './app/translate.provider';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // ✅ Import this
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker'; // ✅ Import this
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideAnimations(),
-    provideTranslate(), provideAnimationsAsync(), provideAnimationsAsync() // ✅ Add this line!
+    provideTranslate(), provideAnimationsAsync(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }) // ✅ Add this line!
   ]
 });
 
