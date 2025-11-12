5323ff42b7a127057daebcfc3428fc90f9300b46
MarinaSlepian <slepianmarina@gmail.com>
2025-11-12 10:29:06 +0200

    remove all payment locks

--- src/app/buttons/buttons-actions-icons.ts
+++ src/app/buttons/buttons-actions-icons.ts
@@ -6,7 +6,7 @@ export const BUTTONS_ACTIONS_ICONS = [
       avatar: 'user-1.png',
       numVideos: '5',
       subtextKey:"subtext.LAUGH",
-      isLocked: true
+      isLocked: false
     },
     {
       id: '2', //CRY
@@ -14,7 +14,7 @@ export const BUTTONS_ACTIONS_ICONS = [
       avatar: 'user-2.png',
       numVideos: '4',
       subtextKey:"subtext.CRY",
-      isLocked: true
+      isLocked: false
     },
     {
       id: '3',//DRINK
